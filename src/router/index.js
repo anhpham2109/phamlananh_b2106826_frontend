2a7df912bc1274aa89b17d9fd3952ff32d49bb66
phamlananh <phamlananh2109200324@gmail.com>
2025-03-09 15:06:27 +0700

    Tao lien he moi

--- src/router/index.js
+++ src/router/index.js
@@ -19,7 +19,11 @@ const routes = [
         props: true // Truyền các biến trong $route.params vào làm props
     },
 
-
+    {
+        path: "/contacts",
+        name: "contact.add",
+        component: () => import("@/views/ContactAdd.vue"),
+    },
 ];
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
