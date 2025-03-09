e5dc2092fa0b15b194bd1c23b95c46e0209a192a
phamlananh <phamlananh2109200324@gmail.com>
2025-03-09 14:40:20 +0700

    Tao trang cap nhat lien he

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -8,14 +8,20 @@ export default {
     methods: {
         updateActiveIndex(index) {
             this.$emit("update:activeIndex", index);
-        }
-    }
+        },
+    },
 };
 </script>
+
 <template>
     <ul class="list-group">
-        <li class="list-group-item" v-for="(contact, index) in contacts" :key="contact._id"
-            :class="{ active: index === activeIndex }" @click="updateActiveIndex(index)">
+        <li
+            class="list-group-item"
+            v-for="(contact, index) in contacts"
+            :key="contact._id"
+            :class="{ active: index === activeIndex }"
+            @click="updateActiveIndex(index)"
+        >
             {{ contact.name }}
         </li>
     </ul>
